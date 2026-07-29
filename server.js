030336d56a7b512ab2d211647270e2edc67fefc8
Eshwar kumar Akula <eshwarkumarakula2@gmail.com>
2026-07-29 23:07:55 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const { neon } = require('@neondatabase/serverless');
 
+const path = require('path');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
+
+// Root route: Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Initialize Neon database client
 const sql = neon(process.env.DATABASE_URL);
